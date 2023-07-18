35403eec511b5ca098f4c3faae2792231d598c09
cartrugs <cartrugs@gmail.com>
2023-07-18 19:03:18 +0200

    Evento mouseenter, mouseleave finalizado

--- js/main.js
+++ js/main.js
@@ -1,72 +1,72 @@
-const bienvenidos = document.querySelector("#bienvenidos");
+const bienvenidos = document.querySelector('#bienvenidos');
 const arrayBanners = [
   {
-    url: "assets/banners/1.jpg",
-    alt: "landscape",
+    url: 'assets/banners/1.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/2.jpg",
-    alt: "landscape",
+    url: 'assets/banners/2.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/3.jpg",
-    alt: "landscape",
+    url: 'assets/banners/3.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/4.jpg",
-    alt: "landscape",
+    url: 'assets/banners/4.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/5.jpg",
-    alt: "landscape",
+    url: 'assets/banners/5.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/6.jpg",
-    alt: "landscape",
+    url: 'assets/banners/6.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/7.jpg",
-    alt: "landscape",
+    url: 'assets/banners/7.jpg',
+    alt: 'landscape',
   },
   {
-    url: "assets/banners/8.jpg",
-    alt: "landscape",
+    url: 'assets/banners/8.jpg',
+    alt: 'landscape',
   },
 ];
 const fragmentBanners = document.createDocumentFragment();
-const recomendados = document.querySelector("#recomendados");
+const recomendados = document.querySelector('#recomendados');
 const arrayViajes = [
   {
-    url: "assets/viajes/viajes-1.jpg",
-    titulo: "viaje 1",
-    alt: "Mujer sobre hamaca en la playa",
-    msg: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, doloribus qui similique necessitatibus.",
+    url: 'assets/viajes/viajes-1.jpg',
+    titulo: 'viaje 1',
+    alt: 'Mujer sobre hamaca en la playa',
+    msg: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, doloribus qui similique necessitatibus.',
   },
   {
-    url: "assets/viajes/viajes-2.jpg",
-    titulo: "viaje 2",
-    alt: "Camino a restaurante sobre el mar",
-    msg: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, doloribus qui similique necessitatibus.",
+    url: 'assets/viajes/viajes-2.jpg',
+    titulo: 'viaje 2',
+    alt: 'Camino a restaurante sobre el mar',
+    msg: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, doloribus qui similique necessitatibus.',
   },
   {
-    url: "assets/viajes/viajes-3.jpg",
-    titulo: "viaje 3",
-    alt: "Múltiples señales en un poste",
-    msg: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, doloribus qui similique necessitatibus.",
+    url: 'assets/viajes/viajes-3.jpg',
+    titulo: 'viaje 3',
+    alt: 'Múltiples señales en un poste',
+    msg: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse, doloribus qui similique necessitatibus.',
   },
 ];
 const fragment = document.createDocumentFragment();
-const destinos = document.querySelector("#destinos");
-const destinoPosible = ["Europa", "América", "África"];
+const destinos = document.querySelector('#destinos');
+const destinoPosible = ['Europa', 'América', 'África'];
 const fragmentDestinos = document.createDocumentFragment();
-
-const sombraActiva= document.querySelector("cajaFotos");
+const cajaFotos = document.querySelectorAll('.figure.background-img');
 
 const pasarBanners = () => {
   const cambiarBanners = arrayBanners[Math.floor(Math.random() * arrayBanners.length)];
-  const cajaBanners = document.createElement("DIV");
-  const imagenBanner = document.createElement("IMG");
-  imagenBanner.src = cambiarBanners.url, cambiarBanners.alt;
+  const cajaBanners = document.createElement('div');
+  const imagenBanner = document.createElement('img');
+  imagenBanner.src = cambiarBanners.url;
+  imagenBanner.alt = cambiarBanners.alt;
   cajaBanners.append(imagenBanner);
   fragmentBanners.append(cajaBanners);
   bienvenidos.append(fragmentBanners);
@@ -74,50 +74,54 @@ const pasarBanners = () => {
 
 const meterFotos = () => {
   arrayViajes.forEach((item) => {
-    const cajaFotos = document.createElement("FIGURE");
-    cajaFotos.classList.add("figure", "background-img");
+    const cajaFotos = document.createElement('FIGURE');
+    cajaFotos.classList.add('figure', 'background-img');
 
-    console.log(cajaFotos);
-
-    const titulo = document.createElement("H3");
+    const titulo = document.createElement('H3');
     titulo.textContent = item.titulo;
 
-    const parrafo = document.createElement("P");
+    const parrafo = document.createElement('P');
     parrafo.textContent = item.msg;
 
-    const cajas = document.createElement("DIV");
+    const cajas = document.createElement('DIV');
 
-    const fotos = document.createElement("IMG");
+    const fotos = document.createElement('IMG');
     fotos.src = item.url;
+    fotos.alt = item.alt;
 
     cajas.append(fotos);
 
-    cajaFotos.append(cajas, titulo, parrafo);
+    cajaFotos.append(cajas);
+    cajaFotos.append(titulo);
+    cajaFotos.append(parrafo);
 
     fragment.append(cajaFotos);
-
-    console.log(fotos);
   });
 
   recomendados.append(fragment);
 };
 
 const cambiarSombra = () => {
-  cajaFotos.addEventListener('mouseenter', (ev) => {
-    cajaFotos.classList.add("sombraActiva")
+
+  cajaFotos.forEach((foto) => {
+    foto.addEventListener('mouseenter', () => {
+      foto.classList.add('sombraActiva');
+    });
+
+    foto.addEventListener('mouseleave', () => {
+      foto.classList.remove('sombraActiva');
+    });
   });
 };
 
 const inputsViajes = () => {
-  console.log(inputsViajes);
   destinoPosible.forEach((item) => {
-    const opciones = document.createElement("OPTION");
+    const opciones = document.createElement('OPTION');
     opciones.textContent = item;
     opciones.value = item;
-    console.log(opciones);
-    fragment.append(opciones);
+    fragmentDestinos.append(opciones);
   });
-  destinos.append(fragment);
+  destinos.append(fragmentDestinos);
 };
 
 pasarBanners();
